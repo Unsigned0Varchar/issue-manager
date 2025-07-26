0c32e31fbe07476bc902e0efbbce9e5d2325e85e
Unsigned0Varchar <123456789abhik@gmail.com>
2025-07-26 17:55:38 +0530

    Rollback to a previous version of app.js file

--- connection.js
+++ connection.js
@@ -1,12 +1,26 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+//Previous connection code commented out
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT || 3306, // Default MySQL port
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+//Local connection code
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Default MySQL port
+  host: 'localhost',
+  user: 'root',
+  password: '123@bhik',
+  database: 'registration_db',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
