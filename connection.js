4cd44b242ba8fdb225f87913f4024812ed95acdc
Unsigned0Varchar <123456789abhik@gmail.com>
2025-07-26 14:14:55 +0530

    Updates

--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST,
-  user: process.env.DB_USER || process.env.MYSQL_ADDON_USER,
-  password: process.env.DB_PASS || process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.DB_NAME || process.env.MYSQL_ADDON_DB,
-  port: process.env.DB_PORT || process.env.MYSQL_ADDON_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
